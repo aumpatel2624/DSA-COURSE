b9f1df0305b20262483dcd2a51bf3c211350aba2
AUM PATEL <aumpatelc36@gmail.com>
2022-05-22 02:19:40 +0530

    added all patterns

--- DSA/pattern1.cpp
+++ DSA/pattern1.cpp
@@ -28,9 +28,8 @@ int main() {
                 cout << " ";
             }
         }
-        cout << endl;
+    cout << endl;
     }
-
     //INVERTED HALF PYRAMID
     cin >> rows;
     for (int i = rows; i >= 1; i--){
@@ -39,5 +38,74 @@ int main() {
         }
         cout << endl;
     }
+    
+    //HALF PYRAMID AFTER 180 ROTATION
+    int n;
+    cin >>n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j<=n-i){
+                cout << " ";
+            }
+            else{
+                cout <<"*";
+            }
+        }
+        cout << endl;
+    }
+    
+    //HALF PYRAMID USING NUMBERS
+    cin >> n;
+    for ( int i = 1; i < n; i++){
+        for (int j = 1; j <= i; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    
+    //FLOYD's TRIANGLE
+    int count = 1;
+    cin >> n;
+    for ( int i = 1; i < n; i++){
+        for (int j = 1; j <= i; j++){
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+
+    //BUTTERFLY PATTERN
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+        cout <<"*";
+        }
+        
+        int space = 2*n-2*i;
+        for(int j = 1; j <= space; j++){
+        cout << " ";
+        }
+        
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i = n; i >= 1; i--){
+        
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        
+        int space=2*n-2*i;
+        for(int j = 1; j<= space; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+    cout << endl;
+    }
+
     return 0;
 }
